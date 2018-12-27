fdd52b1aaf8325d7916edd3ded28824c75b241a0
Otto Kruse <ottokruse@gmail.com>
2018-12-27 15:07:19 +0100

    Added cognito backend

---
+++ cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
@@ -0,0 +1,10 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
+
+export const handler: CognitoUserPoolTriggerHandler = async event => {
+    event.response.autoConfirmUser = true;
+    (event.response as any).autoVerifyEmail = true;
+    return event;
+};
---
+++ cognito/lambda-triggers/verify-auth-challenge-response/verify-auth-challenge-response.ts
@@ -0,0 +1,14 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
+
+export const handler: CognitoUserPoolTriggerHandler = async event => {
+    const expectedAnswer = event.request.privateChallengeParameters!.secretLoginCode; 
+    if (event.request.challengeAnswer === expectedAnswer) {
+        event.response.answerCorrect = true;
+    } else {
+        event.response.answerCorrect = false;
+    }
+    return event;
+};
