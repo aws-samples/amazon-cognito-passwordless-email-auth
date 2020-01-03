c49fba7a377089aecd8965a7e3f3f87d2e676667
Otto Kruse <ottokrus@amazon.nl>
2020-01-03 13:35:03 +0100

    Only update user attribute email_verified if necessary

--- cognito/lambda-triggers/post-authentication/post-authentication.ts
+++ cognito/lambda-triggers/post-authentication/post-authentication.ts
@@ -7,14 +7,16 @@ import { CognitoIdentityServiceProvider } from 'aws-sdk';
 const cup = new CognitoIdentityServiceProvider();
 
 export const handler: CognitoUserPoolTriggerHandler = async event => {
-    const params: CognitoIdentityServiceProvider.AdminUpdateUserAttributesRequest = {
-        UserPoolId: event.userPoolId,
-        UserAttributes: [{
-            Name: 'email_verified',
-            Value: 'true',
-        }],
-        Username: event.userName!,
-    };
-    await cup.adminUpdateUserAttributes(params).promise();
+    if (event.request.userAttributes.email_verified !== 'true') {
+      const params: CognitoIdentityServiceProvider.AdminUpdateUserAttributesRequest = {
+          UserPoolId: event.userPoolId,
+          UserAttributes: [{
+              Name: 'email_verified',
+              Value: 'true',
+          }],
+          Username: event.userName!,
+      };
+      await cup.adminUpdateUserAttributes(params).promise();
+    }
     return event;
 };
