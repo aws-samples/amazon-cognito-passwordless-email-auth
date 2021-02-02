a6c3d23d6c1efbc62ebaa14a1ce18890661e458a
Otto Kruse <ottokrus@amazon.nl>
2021-02-02 17:16:38 +0100

    Deps updated (#31)

--- cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
+++ cognito/lambda-triggers/pre-sign-up/pre-sign-up.ts
@@ -1,9 +1,9 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
-import { CognitoUserPoolTriggerHandler } from 'aws-lambda';
+import { PreSignUpTriggerHandler } from 'aws-lambda';
 
-export const handler: CognitoUserPoolTriggerHandler = async event => {
+export const handler: PreSignUpTriggerHandler = async event => {
     event.response.autoConfirmUser = true;
     return event;
 };
